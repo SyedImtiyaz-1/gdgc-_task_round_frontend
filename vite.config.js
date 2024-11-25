2a8586aaf15bdc1c83bd4f3ae7473200950f08ba
SyedImtiyaz-1 <syedimtiyazali141@gmail.com>
2024-11-25 21:58:18 +0530

    updated .gitignore and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
   },
   resolve: {
     alias: {
-      '@': '/src'
-    }
-  }
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist', // Output folder for the built frontend
+  },
 });
