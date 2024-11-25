91bc26a7469af316d6a42fd566a32c4c7df204fd
SyedImtiyaz-1 <syedimtiyazali141@gmail.com>
2024-11-25 22:35:58 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   build: {
     outDir: 'dist',
   },
-  base: './', // Ensures assets use relative paths
+  base: './', // Ensures relative paths for assets
 });
